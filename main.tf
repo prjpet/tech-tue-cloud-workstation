a87caf28df7405dd417e079101a7f7323242fc47
Peter <Peter@os3.nl>
2021-03-02 19:12:50 +0000

    full main.tf with new vpc

--- main.tf
+++ main.tf
@@ -41,3 +41,32 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"]
 }
+
+resource "aws_vpc" "work-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true" #gives you an internal domain name
+  enable_dns_hostnames = "true" #gives you an internal host name
+  enable_classiclink   = "false"
+  instance_tenancy     = "default"
+
+  tags = {
+    Name = "work-vpc"
+  }
+}
+
+resource "aws_subnet" "work-subnet" {
+  vpc_id                  = aws_vpc.work-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true" #it makes this a public subnet
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "work-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "work-igw" {
+  vpc_id = aws_vpc.work-vpc.id
+  tags = {
+    Name = "work-igw"
+  }
+}
